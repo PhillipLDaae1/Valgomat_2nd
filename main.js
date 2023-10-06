438b0efa4fa217b2bbebc7c84bf7ff9e1faa0ef8
PhillipLDaae1 <e-phidaa@iskule.no>
2023-10-06 17:52:14 +0200

    Andre valgomat, utgangspunkt i Hilde's versjon.

---
+++ main.js
@@ -0,0 +1,555 @@
+
+const questions = [
+{
+    question: 'Vi bør ha høye skatter i Norge, for å ha råd til flere fellesgoder.',
+    enig: {
+        rødt: 0,
+        sv: 0,
+        ap: 1,
+        sp: 0,
+        mdg: 0,
+        krf: 1,
+        venstre: 1,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-uenig': {
+        rødt: 0,
+        sv: 0,
+        ap: 1,
+        sp: 0,
+        mdg: 0,
+        krf: 1,
+        venstre: 1,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-enig': {
+        rødt: 0,
+        sv: 0,
+        ap: 1,
+        sp: 0,
+        mdg: 0,
+        krf: 1,
+        venstre: 1,
+        høyre: 0,
+        frp: 0
+    },
+    uenig: {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 1,
+        frp: 1
+    }
+},
+{
+    question: 'Vi trenger flere private alternativ i blant annet helsesektoren for å få flere valg for pasienter.',
+    enig: {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 1,
+        frp: 1
+    },
+    'litt-uenig': {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 1,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-enig': {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 1,
+        venstre: 1,
+        høyre: 0,
+        frp: 0
+    },
+    uenig: {
+        rødt: 1,
+        sv: 1,
+        ap: 1,
+        sp: 0,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    }
+},
+{
+    question: 'Norge bør innføre strengere innvandringsregler.',
+    enig: {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 1,
+        frp: 1
+    },
+    'litt-uenig': {
+        rødt: 1,
+        sv: 1,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 1,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-enig': {
+        rødt: 0,
+        sv: 0,
+        ap: 1,
+        sp: 1,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    uenig: {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 1,
+        krf: 0,
+        venstre: 1,
+        høyre: 0,
+        frp: 0
+    }
+},
+{
+    question: 'Vi trenger en rusreform der vi avkriminaliserer brukerdoser med cannabis.',
+    enig: {
+        rødt: 1,
+        sv: 1,
+        ap: 0,
+        sp: 0,
+        mdg: 1,
+        krf: 0,
+        venstre: 1,
+        høyre: 1,
+        frp: 0
+    },
+    'litt-uenig': {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 1,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-enig': {
+        rødt: 0,
+        sv: 0,
+        ap: 1,
+        sp: 0,
+        mdg: 0,
+        krf: 1,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    uenig: {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 1
+    }
+},
+{
+    question: 'De rikeste fikk for mye hjelp under koronakrisen.',
+    enig: {
+        rødt: 1,
+        sv: 1,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-uenig': {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 1,
+        venstre: 1,
+        høyre: 0,
+        frp: 1
+    },
+    'litt-enig': {
+        rødt: 0,
+        sv: 0,
+        ap: 1,
+        sp: 1,
+        mdg: 1,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    uenig: {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 1,
+        frp: 0
+    }
+},
+{
+    question: 'Jeg mener vi bør slutte å lete etter mer olje nå.',
+    enig: {
+        rødt: 1,
+        sv: 1,
+        ap: 0,
+        sp: 0,
+        mdg: 1,
+        krf: 0,
+        venstre: 1,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-uenig': {
+        rødt: 0,
+        sv: 0,
+        ap: 1,
+        sp: 0,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-enig': {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 1,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    uenig: {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 1,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 1,
+        frp: 1
+    }
+},
+{
+    question: 'Jeg er bekymret for at internasjonale avtaler fører til at Norge får mindre makt.',
+    enig: {
+        rødt: 1,
+        sv: 1,
+        ap: 0,
+        sp: 1,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-uenig': {
+        rødt: 0,
+        sv: 0,
+        ap: 1,
+        sp: 0,
+        mdg: 0,
+        krf: 1,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-enig': {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 1
+    },
+    uenig: {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 1,
+        krf: 0,
+        venstre: 1,
+        høyre: 1,
+        frp: 0
+    }
+},
+{
+    question: 'Den kristne kulturarven bør påvirke de politiske vedtakene vi gjør.',
+    enig: {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 1,
+        mdg: 0,
+        krf: 1,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-uenig': {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-enig': {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 1,
+        frp: 1
+    },
+    uenig: {
+        rødt: 1,
+        sv: 1,
+        ap: 1,
+        sp: 0,
+        mdg: 1,
+        krf: 0,
+        venstre: 1,
+        høyre: 0,
+        frp: 0
+    }
+},
+{
+    question: 'Sammenslåtte kommuner og fylker bør få oppheve sammenslåingen.',
+    enig: {
+        rødt: 1,
+        sv: 1,
+        ap: 0,
+        sp: 1,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-uenig': {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-enig': {
+        rødt: 0,
+        sv: 0,
+        ap: 1,
+        sp: 0,
+        mdg: 1,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    uenig: {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 1,
+        venstre: 1,
+        høyre: 1,
+        frp: 1
+    }
+},
+{
+    question: 'EØS-avtalen er for dårlig for Norge, vi bør si den opp og prøve å forhandle fram noe bedre.',
+    enig: {
+        rødt: 1,
+        sv: 1,
+        ap: 0,
+        sp: 1,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-uenig': {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 1,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 0
+    },
+    'litt-enig': {
+        rødt: 0,
+        sv: 0,
+        ap: 0,
+        sp: 0,
+        mdg: 0,
+        krf: 0,
+        venstre: 0,
+        høyre: 0,
+        frp: 1
+    },
+    uenig: {
+        rødt: 0,
+        sv: 0,
+        ap: 1,
+        sp: 0,
+        mdg: 1,
+        krf: 1,
+        venstre: 1,
+        høyre: 1,
+        frp: 0
+    }
+}
+];
+
+
+// Her lagrast resultata undervegs - basert på kva du velgjer når du trykker på einig - uenig
+let partyScores = {
+    rødt: 0,
+    sv: 0,
+    ap: 0,
+    sp: 0,
+    mdg: 0,
+    krf: 0,
+    venstre: 0,
+    høyre: 0,
+    frp: 0
+}
+
+const questionT = document.getElementById('question'); // Der me skriv ut spørsmålet
+const btnNext = document.getElementById('btnNext');
+const rbAnswer = document.getElementsByName('answer'); // radiobuttons (fleirtal)
+const inputForm = document.getElementById('valgomatForm');
+
+btnNext.addEventListener('click', nextQuestion);
+
+let qidx = 0; // Hvilken spørsmål er vi på nå (begynner på 0)
+questionT.innerHTML = questions[qidx].question; // Skriver ut det første spørsmålet til HTML
+
+function nextQuestion() {
+    let radioChecked = document.querySelector('input[name="answer"]:checked'); // henter hvilken radioknapp som er valgt
+    
+    if (radioChecked) {
+        calculateResult(qidx, radioChecked.value); // regner ut resultatet
+        qidx++; // øker spørsmåls-ID med 1
+        if (qidx < questions.length) { // Dersom det er fleire spørsmål igjen
+            radioChecked.checked = false;
+            questionT.innerHTML = questions[qidx].question; // Skriver ut neste spørsmål til HTML
+        }
+        else {  // Dersom det ikkje er fleire spørsmål igjen så kan me kalle på funksjonen som oppsummer resultatet
+            inputForm.style.display = 'none'; // Skjuler skjemaet
+            showResult(); 
+        }
+    }
+    else {
+        alert("Du må velge et svaralternativ!");
+    }
+}
+
+// Funksjon som regner ut resultatet, får inn spørsmåls-ID og hvilket svar som er valgt (enig/uenig)
+function calculateResult(qidx, chosen) {
+    console.log("Spørsmåls-ID: " + qidx + ", valgt: " + chosen);
+
+    let partyChoices = questions[qidx][chosen]; // Henter ut partiene og poengene for det valgte svaret
+    console.log("partyChoices: ");
+    console.log(partyChoices);
+
+    for (let party in partyChoices){
+        partyScores[party] += partyChoices[party];
+    }
+
+    console.log("partyScore: ");
+    console.log(partyScores);
+}
+
+
+function showResult() {
+    // Konverter objektet til et array av nøkkel-verdi-par
+    let entries = Object.entries(partyScores);
+
+    // Sorter arrayet etter verdiene i stigende rekkefølge
+    entries.sort ((a, b) => b [1] - a [1]);
+
+    // Konverter arrayet tilbake til et objekt
+    let sortedPartyScores = Object.fromEntries(entries);
+
+    console.log("Sortert etter poengsum: ");
+    console.log(sortedPartyScores); 
+
+    // Skriver ut resultatet til HTML
+    const resultBox = document.getElementById("result");
+    resultBox.innerHTML = "";
+    for (let party in sortedPartyScores) {
+        resultBox.innerHTML += party + ": " + sortedPartyScores[party] + "<br>";
+    }
+}
